55dbd4b97eef5c8aaa4c1c5d0f0f9dd304bc461d
vtjaeger <viniciustjaeger@gmail.com>
2024-02-06 16:27:55 -0300

    created new methods

--- src/main/java/com/site/repositories/UserRepository.java
+++ src/main/java/com/site/repositories/UserRepository.java
@@ -4,8 +4,12 @@ import com.site.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByEmail(String email);
+
+    Optional<UserModel> findByTelephone(String telephone);
 }
